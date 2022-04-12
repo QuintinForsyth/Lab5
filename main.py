f5cbc30708ab00610b1112071a7780489b68e02f
Quintin Forsyth <100383529+QuintinForsyth@users.noreply.github.com>
2022-04-12 22:37:59 +0000

    working final with brute force clear

--- main.py
+++ main.py
@@ -1,26 +1,30 @@
 import random
-import replit
+
 
 def get_int(message):
   x = int(input(message))
   return(x)
+  """
+  gets integer from user
+  """
   
-def play(play_again):
-  if (play_again == "yes" or play_again == "Yes" or play_again == "Y" or play_again == "y"):
+def play():
+  again = input("Correct!\nwould you like to play again (Y or N)?")
+  if (again == "yes" or again == "Yes" or again == "Y" or again == "y"):
+      print("\n"*150)
       print("Reseting random number")
-      replit.clear()
       play_game()
-    
+  else:
+    print("bye );")
 
 def guess_number(random_number, top_range, minimum_range):
   guess = int(input("Guess a number between "+str(minimum_range)+" and "+str(top_range)))
+  
   if (guess == random_number):
-    play_again = input("Correct!\nwould you like to play again (Y or N)?")
-    play(play_again)
+     return 
 
-  elif(guess != random_number):
-    play_again = input("wrong!\nwould you like to play again (Y or N)?")
-    play(play_again)
+  print("Try again")
+  guess_number(random_number, top_range, minimum_range)
 
 
   
@@ -29,6 +33,8 @@ def play_game():
   minimum_range = get_int("Enter the minimum number")
   top_range = get_int("Enter the maximum number")
   random_number = random.randint(minimum_range, top_range)
-  guess_number(random_number, top_range, minimum_range)
+  guess_number(random_number, top_range, minimum_range) 
+  play()
+  
 
 play_game()
